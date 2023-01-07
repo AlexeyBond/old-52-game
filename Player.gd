96444ff8cc07ed40e2d0a4c3aa50715f8fc198b1
A.Bond <alexey.bond.94.55@gmail.com>
2023-01-07 19:04:40 +0600

    harvest animation

--- Player.gd
+++ Player.gd
@@ -1,28 +1,20 @@
 extends Node2D
 
-
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
-
-# Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+	$KinematicBody2D/Hands/AnimationPlayer.play("idle")
 
 var cur_scale = 1
+export var can_move: bool = true
 
-func _physics_process(delta):
-	var horiz = Input.get_axis("ui_left", "ui_right")
-	
-	#$RigidBody2D.apply_impulse(Vector2(0, 0), Vector2(horiz, 0) * 10.0)
-	if (horiz > 0 and cur_scale < 0) or (horiz < 0 and cur_scale > 0):
-		$KinematicBody2D.scale.x = -1
-		cur_scale = -cur_scale
-	$KinematicBody2D.move_and_slide(Vector2(horiz * 100.0, 100))
+func _process(_delta):
+	if Input.is_action_just_pressed("ui_accept"):
+		$KinematicBody2D/Hands/AnimationPlayer.play("grab")
 
+func _physics_process(_delta):
+	if can_move:
+		var horiz = Input.get_axis("ui_left", "ui_right")
+
+		if (horiz > 0 and cur_scale < 0) or (horiz < 0 and cur_scale > 0):
+			$KinematicBody2D.scale.x = -1
+			cur_scale = -cur_scale
+		$KinematicBody2D.move_and_slide(Vector2(horiz * 100.0, 100))
